446f2a4ab9a140d51de9da8b4f5fc6138ebe4c8a
sharanya-a29 <sharanyaa2005@gmail.com>
2026-02-10 18:13:37 +0530

    Delete dashboard.js

--- dashboard.js
+++
@@ -1,6 +0,0 @@
-// Update the 'Tokens Issued' count on the dashboard
-db.collection('stats').doc('tokenCount').onSnapshot((doc) => {
-    if (doc.exists) {
-        document.getElementById('tokenCount').innerText = doc.data().current;
-    }
-});
